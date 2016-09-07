d231580ecdb397cb13a433a869a925ed59b29a26
Foleosy <mywong@outlook.sg>
2016-09-07 22:21:47 +0800

    Added tests for behaviour inspection

--- src/test/java/test/AlphabetizerTest.java
+++ src/test/java/test/AlphabetizerTest.java
@@ -20,4 +20,59 @@ public class AlphabetizerTest {
         assertEquals("test a line", output[2]);
         assertEquals("this is test", output[3]);
     }
+    
+    // See if alphabetical throughout or only first word
+    @Test
+    public void testAlphabeticalSort() throws Exception {
+        Alphabetizer alphabetizer = new Alphabetizer();
+
+        String[] lines = {"a duplicate line with minor differences", "a duplicate line with major differences",
+                "a duplicate line without similarities", "a duplicate line with similarities"};
+        alphabetizer.addLines(lines);
+        String[] output = alphabetizer.getSortedLines();
+        assertTrue(output.length == 4);
+        
+        assertEquals("a duplicate line with major differences", output[0]);
+        assertEquals("a duplicate line with minor differences", output[1]);
+        assertEquals("a duplicate line with similarities", output[2]);
+        assertEquals("a duplicate line without similarities", output[3]);
+    }
+    
+    // Number & Letter combinations sorting order
+    @Test
+    public void testAlphabeticalNumberLetter() throws Exception {
+        Alphabetizer alphabetizer = new Alphabetizer();
+
+        String[] lines = {"1sumplenumber", "2complicatednumbers", "lam3r", "lamer", "placeholder"};
+        alphabetizer.addLines(lines);
+        String[] output = alphabetizer.getSortedLines();
+        assertTrue(output.length == 5);
+
+        assertEquals("1sumplenumber", output[0]);
+        assertEquals("2complicatednumbers", output[1]);
+        assertEquals("lam3r", output[2]);
+        assertEquals("lamer", output[3]);
+        assertEquals("placeholder", output[4]);
+        
+    }
+    
+    // Test if special characters are sorted by ASCII values
+    @Test
+    public void testAlphabeticalSpecial() throws Exception {
+        Alphabetizer alphabetizer = new Alphabetizer();
+
+        String[] lines = {"", ",,,!!", ",,,% ", " ", "??@@", ":;"};
+        alphabetizer.addLines(lines);
+        String[] output = alphabetizer.getSortedLines();
+        assertTrue(output.length == 6);
+        
+        assertEquals("", output[0]);
+        assertEquals(" ", output[1]);
+        assertEquals(",,,!!", output[2]);
+        assertEquals(",,,% ", output[3]);
+        assertEquals(":;", output[4]);
+        assertEquals("??@@", output[5]);
+        
+    }
+    
 }
--- src/test/java/test/CircularShiftTest.java
+++ src/test/java/test/CircularShiftTest.java
@@ -24,4 +24,23 @@ public class CircularShiftTest {
         assertTrue(testSet.contains("Circular Shift Test This"));
         assertTrue(testSet.contains("Shift Test This Circular"));
     }
+    
+    // Testing shifts for words with letters and numbers
+    @Test
+    public void testLetterNumberCircularShifts() throws Exception {
+        CircularShift circularShit = new CircularShift("nuMb3Rs r g00d n0t evIl");
+        String[] shifts = circularShit.getCircularShifts();
+        HashSet<String> testSet = new HashSet<String>();
+        for (String str : shifts) {
+            testSet.add(str);
+        }
+        assertTrue(testSet.size() == 5);
+ 
+        assertTrue(testSet.contains("Numb3rs R G00d N0t Evil"));
+        assertTrue(testSet.contains("R G00d N0t Evil Numb3rs"));
+        assertTrue(testSet.contains("G00d N0t Evil Numb3rs R"));
+        assertTrue(testSet.contains("N0t Evil Numb3rs R G00d"));
+        assertTrue(testSet.contains("Evil Numb3rs R G00d N0t"));
+        
+    }
 }
