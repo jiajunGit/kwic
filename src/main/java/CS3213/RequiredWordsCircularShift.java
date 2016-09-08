e1b328089a26e541d6537bd03b7bd4348780e20c
jiajun <jjgithub@gmail.com>
2016-09-08 15:17:49 +0800

    Fixed getShifts() method to handle lines with more than one required word together with non-required words

--- src/main/java/CS3213/RequiredWordsCircularShift.java
+++ src/main/java/CS3213/RequiredWordsCircularShift.java
@@ -1,6 +1,8 @@
 package CS3213;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RequiredWordsCircularShift {
@@ -23,29 +25,41 @@ public class RequiredWordsCircularShift {
 		
 		String[] words = line.split(DELIMITER);
 		
-		ArrayList<String> resultList = new ArrayList<String>(words.length);
-		for(int i = 0, size = words.length; i < size; ++i) {
-			resultList.add(getShiftedLine( i, words ));
-		}
+		LinkedList<Integer> filteredShifts = new LinkedList<Integer>();
+        for( int i = 0, length = words.length; i < length; ++i ){
+            if( isShiftStartingWithRequiredWord(words, i) ) {
+                filteredShifts.add(i);
+            }
+        }
 		
-		return resultList;
+        ArrayList<String> resultList = new ArrayList<String>(filteredShifts.size());
+        Iterator<Integer> itr = filteredShifts.iterator();
+        while( itr.hasNext() ){
+            resultList.add(getShiftedLine( itr.next(), words ));
+        }
+        
+        return resultList;
 	}
 	
-	 private String getShiftedLine(int index, String[] words) {
-        StringBuilder builder = new StringBuilder();
-
-        for (int i=index;i<words.length;i++) {
-            builder.append(words[i]);
-            builder.append(DELIMITER);
-        }
-        for (int i=0;i<index;i++) {
-            builder.append(words[i]);
-            builder.append(DELIMITER);
-        }
-        if (builder.length() > 0) {
-            builder.deleteCharAt(builder.length() - 1);
-        }
-
-        return builder.toString();
+	private boolean isShiftStartingWithRequiredWord( String[] line, int firstWordIndex ) {
+        return _requiredWords.contains(line[firstWordIndex]);
     }
+	
+	private String getShiftedLine(int index, String[] words) {
+	    StringBuilder builder = new StringBuilder();
+	
+	    for (int i=index;i<words.length;i++) {
+	        builder.append(words[i]);
+	        builder.append(DELIMITER);
+	    }
+	    for (int i=0;i<index;i++) {
+	        builder.append(words[i]);
+	        builder.append(DELIMITER);
+	    }
+	    if (builder.length() > 0) {
+	        builder.deleteCharAt(builder.length() - 1);
+	    }
+	
+	    return builder.toString();
+	}
 }
