ee7a77af10861fecaadecebe0c53f8a666593695
jiajun <jjgithub@gmail.com>
2016-09-08 14:22:05 +0800

    Corrected wrong test parameters

--- src/test/java/test/RequiredWordsCircularShiftTest.java
+++ src/test/java/test/RequiredWordsCircularShiftTest.java
@@ -22,6 +22,6 @@ public class RequiredWordsCircularShiftTest {
 	public void testCreateWithNullRequiredWords() {
 		
 		CheckableSpecialWordsCollection requiredWords = RequiredWords.create();
-		RequiredWordsCircularShift shifter = RequiredWordsCircularShift.create(requiredWords);
+		RequiredWordsCircularShift shifter = RequiredWordsCircularShift.create(null);
 	}
 }
