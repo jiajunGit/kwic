67a79442455280a87bf5275fade2101dec80dcab
jiajun <jjgithub@gmail.com>
2016-09-08 15:07:35 +0800

    Fixed getShifts method to handle lines that consist of only required words (> 1 required words)

--- src/main/java/CS3213/RequiredWordsCircularShift.java
+++ src/main/java/CS3213/RequiredWordsCircularShift.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class RequiredWordsCircularShift {
 
+	private static final String DELIMITER = " ";
 	private CheckableSpecialWordsCollection _requiredWords;
 	
 	private RequiredWordsCircularShift( CheckableSpecialWordsCollection requiredWords ) {
@@ -20,9 +21,31 @@ public class RequiredWordsCircularShift {
 		
 		assert(_requiredWords.isEmpty() == false);
 		
-		List<String> shifts = new ArrayList<String>();
-		shifts.add(line);
+		String[] words = line.split(DELIMITER);
 		
-		return shifts;
+		ArrayList<String> resultList = new ArrayList<String>(words.length);
+		for(int i = 0, size = words.length; i < size; ++i) {
+			resultList.add(getShiftedLine( i, words ));
+		}
+		
+		return resultList;
 	}
+	
+	 private String getShiftedLine(int index, String[] words) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i=index;i<words.length;i++) {
+            builder.append(words[i]);
+            builder.append(DELIMITER);
+        }
+        for (int i=0;i<index;i++) {
+            builder.append(words[i]);
+            builder.append(DELIMITER);
+        }
+        if (builder.length() > 0) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+
+        return builder.toString();
+    }
 }
